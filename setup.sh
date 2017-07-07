3972cc87379c1e6ead5218a4f9ca6d2f232c464e
Haoyu Wu <cq_wu@foxmail.com>
2017-07-07 17:02:18 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
-	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git
+	sudo apt-get install -y vim vim-gnome ctags xclip astyle python-setuptools python-dev git vim-nox
 elif which yum >/dev/null; then
-	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel	
+	sudo yum install -y gcc vim git ctags xclip astyle python-setuptools python-devel vim-nox
 fi
 
 ##Add HomeBrew support on  Mac OS
@@ -21,7 +21,7 @@ mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "ma6174正在努力为您安装bundle程序" > ma6174
+echo "正在努力为女装dalao安装bundle程序" > ma6174
 echo "安装完毕将自动退出" >> ma6174
 echo "请耐心等待" >> ma6174
 vim ma6174 -c "BundleInstall" -c "q" -c "q"
