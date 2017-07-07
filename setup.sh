0e640af07de0240da64550b3a21a84a906678406
Haoyu Wu <cq_wu@foxmail.com>
2017-07-07 21:37:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
+echo "安装将花费一定时间，请女装dalao耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim-nox-py2 ctags xclip astyle python-setuptools python-dev git
 elif which yum >/dev/null; then
